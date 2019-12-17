259eae32eb649696199807e00c7657e5e1cce32a
lillalysmasken3 <linuseriklarsson@hotmail.com>
2019-12-17 09:00:46 +0100

    might have destroyed it

--- blinky_med_switch/main_with_switch.c
+++ blinky_med_switch/main_with_switch.c
@@ -1,12 +1,9 @@
-#include <windows.h>
-
-
 #define GPIO_DIR_F_RGB        (*((volatile unsigned long *)0x4005D400)) //gpio direction //offset 0x400
 #define GPIO_DEN_F_RGB       (*((volatile unsigned long *)0x4005D51C)) //Data enable //offset 0x51C
 #define SYSCTL_RCGCGPIO       (*((volatile unsigned long *)0x400FE608)) //Run mode Clock Gate Control //offset 0x608
 #define GPIO_DATA_F_RGB		  (*((volatile unsigned long *)0x4005D000)) //send data to this one //offset 0x0
 
-
+int sleep(int times);
 void o(int color,int num);
 void s(int color,int num);
 
@@ -24,7 +21,7 @@ while (1)
 	int red=0x02,green=0x04,blue=0x08,white=0xE;
 
 	
-	if(GPIO_DATA_F_RGB=^0xFE)
+	if((GPIO_DATA_F_RGB & 0x01)==0x01)
 	{	
 		sBeep(green,1);
 		sBeep(red,3);
@@ -35,7 +32,7 @@ while (1)
 
 void sBeep(int color,int num);
 {
-	off=0x0
+	int off=0x0;
 	for (int i=0;i<num;i++)
 	{
 	GPIO_DATA_F_RGB&=1;
@@ -49,7 +46,7 @@ void sBeep(int color,int num);
 
 void lBeep(int color,int num);
 {
-	off=0x0
+	int off=0x0;
 	for (int i=0;i<num;i++)
 	{
 	GPIO_DATA_F_RGB&=1;
@@ -58,5 +55,21 @@ void lBeep(int color,int num);
 	GPIO_DATA_F_RGB&=1;
 	GPIO_DATA_F_RGB=off;
 	sleep(2);
+	}
+}
+
+int sleep (int times)
+{
+	int y=0;period=1000000000;
+	
+	
+	for (int i=0; i<times; i++)
+	{
+		while (y<period)
+		{
+			int x=y%3;
+			x*250000;
+			y++;
+		}
 	}
 }
